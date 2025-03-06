fee716967aa3a185dd20fb86fde76ac614193348
rusov <rusov63@yahoo.com>
2025-03-06 15:22:39 +0400

    1ый коммит от 06.03.2025

--- Local/check_path.py
+++ Local/check_path.py
@@ -1,6 +1,7 @@
+import asyncio
 import os
-from Local.globals import counter, local_user_paths
-from Local.file_utils import count_files_in_directory
+from Local.globals import counter, stored_user_paths
+from Local.file_utils import print_count_files_and_directories
 
 
 # globals import counter, local_user_paths СЧЕТЧИКИ И хранение путей!
@@ -12,22 +13,16 @@ async def check_path_file_folder(path_local: str) -> bool:
     Функция проверяет существование указанного пути и определяет, является ли он файлом или директорией.
     В зависимости от типа пути выполняются следующие действия:
     - Для файла: добавляет путь в список и увеличивает счетчик файлов
-    - Для директории: добавляет путь в список, увеличивает счетчик директорий и подсчитывает количество файлов внутри
+    - Для директории: добавляет путь в список, увеличивает счетчик директорий и подсчитывает количество
+    файлов внутри
 
     Параметры: path_local (str): Путь к файлу или директории для проверки
 
     Возвращает: None
-
     Примечания:
-        - Для директорий автоматически добавляется завершающий слеш, если его нет
         - Дублирующиеся пути не добавляются повторно
         - Функция выводит информационные сообщения о результатах проверки
     """
-
-    # Проверяем существует ли путь
-
-
-async def check_path_file_folder(path_local: str) -> bool:
     try:
         if not os.path.exists(path_local):
             print(f'Путь {path_local} не существует.')
@@ -39,43 +34,43 @@ async def check_path_file_folder(path_local: str) -> bool:
             return False
 
         # Проверяем является ли путь ФАЙЛОМ
-        elif os.path.isfile(path_local):
-            if path_local not in local_user_paths.paths:
-                local_user_paths.append(path_local)
+        elif await is_file(path_local):
+            if path_local not in stored_user_paths.paths:
+                stored_user_paths.append(path_local)
                 counter.local_file += 1
-                print(f'Файл: {path_local.split('/')[-1]} успешно добавлен.')
+                print(f'Файл: "{path_local.split('/')[-1]}" успешно добавлен.')
+                print('*' * 40)
             else:
-                print(f"Файл с указанным путем уже был добавлен ранее.")
+                print(f"Файл с указанным путем уже существует в списке.")
+                print('*' * 50)
                 return False
 
         # Проверяем является ли путь ДИРЕКТОРИЕЙ
-        elif '/' != path_local[-1]:  # Если путь не заканчивается слешем
-            path_local = path_local + '/'  # Добавляем слеш в конец
+        elif await is_directory(path_local):
+            if path_local[-1] == '/':
+                # Проверяем существует ли путь в списке global class PATH
+                if path_local not in stored_user_paths.paths and path_local.rstrip('/') not in stored_user_paths.paths:
+                    stored_user_paths.append(path_local)  # добавляем путь в global class PATH
+                    counter.local_dir += 1  # Прибавляем счетчик global class Counter
+                    # подсчитывает количество файлов в указанной директории.
+                    await print_count_files_and_directories(path_local)
+                else:
+                    print(f"Папка с указанным путем уже существует в списке.")
+                    print('*' * 50)
+                    return False
 
-            if path_local not in local_user_paths.paths:  # Проверяем существует ли путь в списке global class PATH
+            elif path_local[-1] != '/':
+                # Проверяем существует ли путь в списке global class PATH
+                if path_local + '/' not in stored_user_paths.paths and path_local not in stored_user_paths.paths:
+                    stored_user_paths.append(path_local)
+                    counter.local_dir += 1  # Прибавляем счетчик global class Counter
+                    # подсчитывает количество файлов в указанной директории.
+                    await print_count_files_and_directories(path_local)
+                else:
+                    print(f"Папка с указанным путем уже существует в списке.")
+                    print('*' * 50)
+                    return False
 
-                local_user_paths.append(path_local)  # добавляем путь в global class PATH
-                counter.local_dir += 1  # Прибавляем счетчик global class Counter
-
-                # подсчитывает количество файлов в указанной директории.
-                await count_files_in_directory(path_local)
-
-            elif path_local in local_user_paths.paths:
-                print(f"Папка с указанным путем уже была добавлена ранее.")
-                return False
-
-        elif '/' == path_local[-1]:  # Если путь заканчивается слешем
-
-            if path_local not in local_user_paths.paths:  # Проверяем существует ли путь в списке PATH
-
-                local_user_paths.append(path_local)  # добавляем путь в global class PATH
-                counter.local_dir += 1  # Прибавляем счетчик global class Counter
-
-                # подсчитывает количество файлов в указанной директории.
-                await count_files_in_directory(path_local)
-            else:
-                print(f"Папка с указанным путем уже была добавлена ранее.")
-                return False
         else:
             print(f"Путь '{path_local}' существует, но не является файлом или папкой.")
             return False
@@ -83,3 +78,37 @@ async def check_path_file_folder(path_local: str) -> bool:
     except Exception as e:
         print(f"Непредвиденная ошибка: {e}")
         return False
+
+
+async def is_directory(path_local: str) -> bool:
+    """
+    Проверяет, является ли указанный путь директорией.
+
+    Эта асинхронная функция использует 'os.path.isdir' для проверки,
+    является ли путь 'path_local' директорией. 'os.path.isdir'
+    вызывается в отдельном потоке с помощью 'asyncio.to_thread',
+    что позволяет избежать блокировки основного цикла событий.
+
+    Параметры:
+    path_local (str): Путь к проверяемой директории.
+    Возвращает:
+    bool: True, если путь является директорией, иначе False.
+    """
+    return await asyncio.to_thread(os.path.isdir, path_local)
+
+
+async def is_file(path_local: str) -> bool:
+    """
+    Проверяет, является ли указанный путь файлом.
+
+    Эта асинхронная функция использует 'os.path.isfile' для проверки,
+    является ли путь `path_local` файлом. 'os.path.isfile'
+    вызывается в отдельном потоке с помощью 'asyncio.to_thread',
+    что позволяет избежать блокировки основного цикла событий.
+
+    Параметры:
+    path_local (str): Путь к проверяемому файлу.
+    Возвращает:
+    bool: True, если путь является файлом, иначе False.
+    """
+    return await asyncio.to_thread(os.path.isfile, path_local)
