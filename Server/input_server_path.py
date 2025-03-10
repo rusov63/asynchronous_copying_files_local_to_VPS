5ac0698f69c39fb730a69c04e4fdbba135f0a45e
rusov <rusov63@yahoo.com>
2025-03-10 19:48:59 +0400

    3ый коммит от 10.03.2025.

--- Server/input_server_path.py
+++ Server/input_server_path.py
@@ -1,11 +1,32 @@
+import asyncio
+import logging
 import sys
 import re
 
-FOLDER_PATH = '/usr/my_files/'  # Путь по умолчанию
+import aioconsole
+import asyncssh
 
-FIRST_MESSAGE = f'\nУкажите путь к папке на сервере (по умолчанию {FOLDER_PATH}), начиная с символа "/".'
 
-SECOND_MESSAGE = f'Для отмены копирования наберите - "exit".\n'
+# Настройка логирования
+logging.basicConfig(
+    filename='app_info.log',  # Имя файла для записи логов
+    filemode='a',              # Режим записи: 'a' для добавления
+    level=logging.INFO,        # Уровень логирования
+    format='%(asctime)s - %(levelname)s - %(message)s - %(name)s - %(filename)s:%(lineno)d'
+)
+
+FOLDER_PATH = '/my_folder'  # Путь по умолчанию
+EXIT_COMMAND = "exit"
+
+
+FIRST_MESSAGE = (
+    f'\nДля копирования укажите путь к папке на сервере (по умолчанию путь {FOLDER_PATH}),\n'
+    f'начиная с символа "/" (например, /home/my_papka/).'
+)
+
+SECOND_MESSAGE = (
+    "Для отмены копирования наберите - \"exit\".\n"
+)
 
 
 async def get_server_path_user() -> str:
@@ -25,6 +46,7 @@ async def get_server_path_user() -> str:
     Raises:
         KeyboardInterrupt: При прерывании программы пользователем (Ctrl+C).
         OSError: При ошибках, связанных с доступом к файловой системе.
+        CancelledError: При прерывании отменяется внешними факторами или системой.
 
     Usage:
         path = await get_server_path_user()
@@ -36,25 +58,28 @@ async def get_server_path_user() -> str:
     while True:
 
         try:
-            user_input = input("Укажите путь: ").strip()
+            user_input = await aioconsole.ainput("Введите путь к папке: ")
+            user_input = user_input.strip().lower()
 
             # Завершение программы
-            if user_input.lower() == "exit":
+            if user_input == EXIT_COMMAND:
+                logging.info("Прерывание пользователем.")
                 print("Завершение программы.")
                 sys.exit()
 
             # Проверяем путь на пустую строку
-            elif not user_input.strip():
-                print("Путь не может быть пустым\nВыбрать путь по умолчанию? (да, нет)")
-                user = input().lower()
-                if user == 'да' or user == '':
+            elif not user_input:
+                print("\nПуть не может быть пустым")
+                user = await aioconsole.ainput("Выбрать путь по умолчанию? (да, нет): ")
+                user = user.lower()
+                if user in ['да', '']:
                     print(f'Используется путь: {FOLDER_PATH}')
                     return FOLDER_PATH
                 else:
                     continue
 
             # Проверяем начало пути с '/' или длину ввода не меньше или равно 3
-            elif '/' not in user_input[0] or len(user_input) <= 3:
+            elif '/' not in user_input[0] or len(user_input) <= 2:
                 print(f"Неверный формат пути. Пример: {FOLDER_PATH}")
                 continue
 
@@ -62,19 +87,24 @@ async def get_server_path_user() -> str:
                 print(f'Путь корректен, введенный путь: {user_input}')
                 return user_input
             else:
-                print("Путь содержит недопустимые символы. Используйте только английские буквы, подчеркивания и дефисы.")
+                print("Путь содержит недопустимые символы. "
+                      "Используйте только английские буквы, подчеркивания и дефисы.")
                 continue
 
         except KeyboardInterrupt:
             print("\nПрерывание пользователем.")
             sys.exit(1)
 
-        except OSError:
-            print("Ошибка при работе с папкой")
+        except (asyncssh.Error, OSError) as e:
+            print("\nОшибка при работе с папкой")
             continue
 
+        except asyncio.CancelledError:
+            print("\nОперация была отменена.")
+            sys.exit(1)
+
         except Exception as e:
-            print(f"Произошла ошибка: {e}")
+            print(f"\nПроизошла ошибка: {e}")
             continue
 
 
@@ -85,7 +115,7 @@ def is_valid_path(path: str) -> bool:
 
     Функция использует регулярное выражение для проверки того,
     что путь состоит только из разрешённых символов: английских букв,
-    подчеркиваний и дефисов, а также слешей.
+    подчеркиваний, дефисов, цифр, а также флеш.
 
     Args:
         path (str): Путь, который необходимо проверить.
@@ -97,6 +127,8 @@ def is_valid_path(path: str) -> bool:
         is_valid_path("/home/user/documents")  # Вернёт True
         is_valid_path("home/user/documents")    # Вернёт False
     """
-    pattern = r'^[a-zA-Z/_-]+$'
+    pattern = r'^[a-zA-Z0-9/_-]+$'
     return bool(re.match(pattern, path))
 
+
+asyncio.run(get_server_path_user())
