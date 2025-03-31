fcf7d3993a5d131325bd8d15499acae24d01fd9d
rusov <rusov63@yahoo.com>
2025-03-31 12:45:42 +0400

    update

--- config.py
+++ config.py
@@ -6,8 +6,27 @@ load_dotenv(override=True)
 
 
 # Получаем данные из переменных окружения
-HOST = os.getenv("HOST")
-USERNAME = os.getenv("USERNAME")
-PASSWORD = os.getenv("PASSWORD")
-ENABLE_PASSWORD = os.getenv("ENABLE_PASSWORD")
-ENCRYPTION_ALGS = os.getenv("ENCRYPTION_ALGS")
+HOST: str = os.getenv("HOST")
+USERNAME: str = os.getenv("USERNAME")
+PASSWORD: str = os.getenv("PASSWORD")
+ENCRYPTION_ALGS: str = os.getenv("ENCRYPTION_ALGS")
+
+
+class ConfigServer():
+    HOST = str(os.getenv("HOST"))
+    USERNAME = str(os.getenv("USERNAME"))
+    PASSWORD =  str(os.getenv("PASSWORD"))
+    ENCRYPTION_ALGS = os.getenv("ENCRYPTION_ALGS")
+
+
+TIMEOUTERROR_MESSAGE: str = "\nСервер не отвечает на запросы."
+
+CANCELLED_MESSAGE: str = "\nОперация была отменена пользователем."
+
+ERROR_MESSAGE: str = "\nОшибка выполнения команды: {e}."
+
+EXCEPTION_MESSAGE: str = "\nПроизошла ошибка: {e}."
+
+PERMISSION_MESSAGE: str = "\nНет прав для подключения к серверу."
+
+CHANNEL_MESSAGE: str = "\nОшибка открытия канала."
